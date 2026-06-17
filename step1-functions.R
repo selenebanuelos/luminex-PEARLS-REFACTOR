8730c25e2c6e073f606bb2dfb47f52308b0cd76b
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-06-16 17:00:46 -0700

    Finished refactoring step 1 of pipeline
    Step 1 takes raw csv files from xPONENT software and reformats them into useable data frames

--- step1-functions.R
+++ step1-functions.R
@@ -7,9 +7,9 @@
 reformatRawData <- function(path, 
                             plasma # dataframe with plasma availability
                             ) {
-    # for testing
-    path <- "data-raw/T2_6plex"
-    plasma <- plasma
+    # # for testing
+    # path <- "data-raw/T2_6plex"
+    # plasma <- plasma
     
     # import all csv in path as data frames into a list
     data_list <- importDataAsList(path, "\\.csv") %>%
@@ -28,14 +28,14 @@ reformatRawData <- function(path,
         lapply(extractBatchDateAnalytes) %>%
         # extract net mfi, %CV, std concentration, and dilution factor data
         lapply(extractAndCombineData) %>% 
-        lapply(filter, specimenID != 'blank') %>% # left off here 6/15/2026
-        lapply(addTimePointCol, plasma) %>%
-        lapply(rename, dilution_factor = 'Dilution Factor') %>%
-        lapply(compareSampleToStd) %>%
-        # change all NaN to NA in dataframe
-        lapply(function(df) mutate_all(df, ~ifelse(is.nan(.), NA, .)))
+        # remove empty wells
+        lapply(filter, specimenID != 'blank') %>%
+        # Merge visit number to cleaned data (do i need this here?)
+        lapply(addVisitnum, plasma) %>%
+        # do i need to do this?
+        lapply(rename, dilution_factor = 'Dilution Factor')
     
-    check <- data_list[[1]]
+    #check <- data_list[[1]]
 }
 
 # import all data within directory into a list of data frames with file names
@@ -184,199 +184,199 @@ extractAndCombineData <- function(df) {
     #check <- merged0[[1]]
 }
 
-# Merge visit number to cleaned data by specimen ID. 
-# Purpose of this is to produce column that acts as a sanity check that all the data contained in final cleaned data .csv is from the expected time point
-addTimePointCol <- function(df, plasma) {
+# Purpose of this is to add column that acts as a sanity check that all the 
+# data contained in final cleaned data .csv is from the expected time point
+addVisitnum <- function(df, plasma) {
     merged <- plasma %>%
         rename(specimenID = "specimenid", subjectID = "subjectid") %>%
         mutate(specimenID = as.character(specimenID)) %>%
         right_join(., df, by="specimenID")
 }
 
-# Filter the number of standard points given analyte and add binary variable that tells whether or not sample net mfi falls above lowest standard
-# The final data list is ready for curve fitting/estimating concentration as well as checking for sample reruns
-compareSampleToStd <- function(df) {
-    #df <- check
-    
-    # separate analyte names contained in string to vector of names
-    analytes <- strsplit(df[1, 'analytes'], ",") %>%
-        unlist() %>%
-        str_trim() # remove whitespaces
-    
-    id_vars <- c('subjectID', 'specimenID', 'plasma', 'visitnum', 'date', 'batch', 'Location', 'file_name', 'analytes', 'dilution_factor')
-    
-    final_data <- sapply(analytes, function(df) df = data.frame(x = df) ) %>%# create list of data frames, each with data for one analyte
-        lapply(., subsetAnalyte, df) %>%
-        lapply(removeVarsAnalytePrefix) %>%
-        lapply(filterStandards) %>%
-        lapply(invalidateResultsCV, id_vars) %>%
-        lapply(createLowestStdVar) %>%
-        lapply(createBelowLowStdBinary) %>%
-        lapply(createHighestStdVar) %>%
-        lapply(createAboveHighStdBinary) %>%
-        lapply(appendVarsAnalytePrefix, id_vars) %>%
-        lapply(select, -contains('_analyte')) %>%
-        reduce(full_join, by = id_vars)
-    
-    #check2 <- final_data[[2]]
-}
-
-# append given variables with analyte prefix
-appendVarsAnalytePrefix <- function(df, not_cols) {
-    #df <- analyte_df[[1]]
-    
-    analyte <- as.character(df[1, 'analyte'])
-    
-    cols <- names(df)[!names(df) %in% not_cols]
-    
-    appended_df <- df %>% 
-        rename_with(~paste0(analyte, '_', .x), cols)
-}
-
-# create new binary variable which:
-# equals 1 if sample net mfi is equal to or above net mfi of highest standard in dataframe
-# equals 0 if sample net mfi falls below net mfi of highest standard in dataframe
-createAboveHighStdBinary <- function(df){
-    #df <- final_data[[1]]
-    
-    # compare sample net mfi to net mfi of highest standard
-    df_var_added <- df %>% 
-        mutate('above_highest_std' = case_when(net_mfi > highest_std_mfi ~ 1,
-                                               net_mfi <= highest_std_mfi ~ 0)) 
-}
-
-# create a new variable in dataframe populated with the net mfi value of the highest standard in dataframe
-createHighestStdVar <- function(df){
-    #df <-  check2
-    
-    # std_1 <- with(df, grepl('Standard1', specimenID))
-    rows_with_stds <- with(df, grepl('Standard', specimenID))
-    
-    highest_std <- df %>%
-        filter(rows_with_stds) %>%
-        select(net_mfi) %>%
-        max()
-    
-    df_var_added <- df %>%
-        mutate(highest_std_mfi = highest_std)
-}
-
-# create new binary variable which:
-# equals 1 if sample net mfi is equal to or above net mfi of lowest standard in dataframe
-# equals 0 if sample net mfi falls below net mfi of lowest standard in dataframe
-createBelowLowStdBinary <- function(df){
-    #df <- final_data[[1]]
-    
-    # compare sample net mfi to net mfi of lowest standard
-    df_var_added_1 <- df %>% 
-        mutate('below_lowest_std' = case_when(net_mfi >= lowest_std_mfi ~ 0,
-                                              net_mfi < lowest_std_mfi ~ 1,
-                                              is.na(lowest_std_mfi) ~ NA))
-}
-
-# create a new variable in dataframe populated with the net mfi value of the lowest standard in dataframe
-createLowestStdVar <- function(df){
-    #df <-  final_data[[1]]
-    
-    rows_with_stds <- with(df, grepl('Standard', specimenID))
-    
-    lowest_std <- df %>%
-        mutate_all(~ifelse(is.nan(.), NA, .)) %>%
-        filter(rows_with_stds) %>%
-        select('net_mfi') %>%
-        tidyr::drop_na() %>%
-        min()
-    
-    df_var_added <- df %>%
-        # mutate(lowest_std_mfi = ifelse(lowest_std >= 10, lowest_std, 'lowest std falls below 10 net mfi')) %>%
-        mutate(notes = ifelse(lowest_std < 10, 
-                              paste0(df[1, 'analyte'], " lowest standard falls below 10 net mfi"), 
-                              NA)) %>%
-        mutate(lowest_std_mfi = ifelse(lowest_std >= 10,
-                                       lowest_std, NA))
-}
-
-# if cv is NA or cv > 20 then invalidate results - one analyte dataframe should be passed in. Also need to specify identification variables so they don't get invalidated.
-invalidateResultsCV <- function(one_analyte_df, id_vars) {
-    result_vars <- setdiff(names(one_analyte_df), c(id_vars, 'cv', 'analyte'))
-    
-    check <- one_analyte_df %>%
-        mutate(across(result_vars, ~ case_when(cv > 20 ~ NA, 
-                                               is.na(cv) ~ NA,
-                                               TRUE ~ .)))
-}
-
-# THIS FUNCTION IS GROSS, FIX THIS WHEN YOU HAVE TIME
-# check out: https://stackoverflow.com/questions/55059761/r-mutate-multiple-columns-with-ifelse-condition
-# Function that select correct number of standard points to include in curve, given analyte name
-filterStandards <- function(df) {
-    stds_5_6_7 <- c("Standard5", "Standard6", "Standard7")# keep standards 1-4
-    stds_6_7 <- c("Standard6", "Standard7")# keep standards 1-5
-    std_7 <- c("Standard7")# keep standards 1-6
-    
-    analyte <- df[1, 'analyte']
-    
-    filtered_stds <- if (analyte == 'CHI3L1'){
-        filter(df, (!specimenID %in% stds_6_7))
-    } 
-    else if (analyte == 'BDNF') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'MPO') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'CRP') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'IGFBP-1') {
-        filter(df, (!specimenID %in% stds_6_7))
-    }
-    else if (analyte == 'IGFBP-3') {
-        filter(df, (!specimenID %in% stds_5_6_7))
-    }
-    else if (analyte == 'Leptin') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'TNF-a') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'VEGF') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'IL-1B') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'IL-6') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else if (analyte == 'IL-10') {
-        filter(df, (!specimenID %in% std_7))
-    }
-    else {
-        df
-    }
-}
-
-# remove analyte prefix from vars to make manipulations easier
-removeVarsAnalytePrefix <- function(df) {
-    #df <- analyte_df[[1]]
-    analyte <- as.character(df[1, 'analyte'])
-    
-    prefix <- paste0(analyte, '_')
-    
-    remove_df <- df %>%
-        rename_all(~str_replace(., paste0(analyte, '_'), ""))
-}
-
-# function that creates separate data frames for each analyte
-subsetAnalyte <- function(df, plate_data) {
-    analyte_name <- df
-    
-    analyte_df <- plate_data %>%
-        select(c('subjectID', 'specimenID', 'plasma', 'visitnum', 'date', 'batch', 'Location', 'dilution_factor', 'file_name', 'analytes', contains(analyte_name))) %>%
-        #rename_at(vars(contains(analyte_name)), ~ str_replace(., paste0(analyte_name, "_"), "")) %>%
-        mutate(analyte = analyte_name)
-}
+# # Filter the number of standard points given analyte and add binary variable that tells whether or not sample net mfi falls above lowest standard
+# # The final data list is ready for curve fitting/estimating concentration as well as checking for sample reruns
+# compareSampleToStd <- function(df) {
+#     #df <- check
+#     
+#     # separate analyte names contained in string to vector of names
+#     analytes <- strsplit(df[1, 'analytes'], ",") %>%
+#         unlist() %>%
+#         str_trim() # remove whitespaces
+#     
+#     id_vars <- c('subjectID', 'specimenID', 'plasma', 'visitnum', 'date', 'batch', 'Location', 'file_name', 'analytes', 'dilution_factor')
+#     
+#     final_data <- sapply(analytes, function(df) df = data.frame(x = df) ) %>%# create list of data frames, each with data for one analyte
+#         lapply(., subsetAnalyte, df) %>%
+#         lapply(removeVarsAnalytePrefix) %>%
+#         lapply(filterStandards) %>%
+#         lapply(invalidateResultsCV, id_vars) %>%
+#         lapply(createLowestStdVar) %>%
+#         lapply(createBelowLowStdBinary) %>%
+#         lapply(createHighestStdVar) %>%
+#         lapply(createAboveHighStdBinary) %>%
+#         lapply(appendVarsAnalytePrefix, id_vars) %>%
+#         lapply(select, -contains('_analyte')) %>%
+#         reduce(full_join, by = id_vars)
+#     
+#     #check2 <- final_data[[2]]
+# }
+# 
+# # append given variables with analyte prefix
+# appendVarsAnalytePrefix <- function(df, not_cols) {
+#     #df <- analyte_df[[1]]
+#     
+#     analyte <- as.character(df[1, 'analyte'])
+#     
+#     cols <- names(df)[!names(df) %in% not_cols]
+#     
+#     appended_df <- df %>% 
+#         rename_with(~paste0(analyte, '_', .x), cols)
+# }
+# 
+# # create new binary variable which:
+# # equals 1 if sample net mfi is equal to or above net mfi of highest standard in dataframe
+# # equals 0 if sample net mfi falls below net mfi of highest standard in dataframe
+# createAboveHighStdBinary <- function(df){
+#     #df <- final_data[[1]]
+#     
+#     # compare sample net mfi to net mfi of highest standard
+#     df_var_added <- df %>% 
+#         mutate('above_highest_std' = case_when(net_mfi > highest_std_mfi ~ 1,
+#                                                net_mfi <= highest_std_mfi ~ 0)) 
+# }
+# 
+# # create a new variable in dataframe populated with the net mfi value of the highest standard in dataframe
+# createHighestStdVar <- function(df){
+#     #df <-  check2
+#     
+#     # std_1 <- with(df, grepl('Standard1', specimenID))
+#     rows_with_stds <- with(df, grepl('Standard', specimenID))
+#     
+#     highest_std <- df %>%
+#         filter(rows_with_stds) %>%
+#         select(net_mfi) %>%
+#         max()
+#     
+#     df_var_added <- df %>%
+#         mutate(highest_std_mfi = highest_std)
+# }
+# 
+# # create new binary variable which:
+# # equals 1 if sample net mfi is equal to or above net mfi of lowest standard in dataframe
+# # equals 0 if sample net mfi falls below net mfi of lowest standard in dataframe
+# createBelowLowStdBinary <- function(df){
+#     #df <- final_data[[1]]
+#     
+#     # compare sample net mfi to net mfi of lowest standard
+#     df_var_added_1 <- df %>% 
+#         mutate('below_lowest_std' = case_when(net_mfi >= lowest_std_mfi ~ 0,
+#                                               net_mfi < lowest_std_mfi ~ 1,
+#                                               is.na(lowest_std_mfi) ~ NA))
+# }
+# 
+# # create a new variable in dataframe populated with the net mfi value of the lowest standard in dataframe
+# createLowestStdVar <- function(df){
+#     #df <-  final_data[[1]]
+#     
+#     rows_with_stds <- with(df, grepl('Standard', specimenID))
+#     
+#     lowest_std <- df %>%
+#         mutate_all(~ifelse(is.nan(.), NA, .)) %>%
+#         filter(rows_with_stds) %>%
+#         select('net_mfi') %>%
+#         tidyr::drop_na() %>%
+#         min()
+#     
+#     df_var_added <- df %>%
+#         # mutate(lowest_std_mfi = ifelse(lowest_std >= 10, lowest_std, 'lowest std falls below 10 net mfi')) %>%
+#         mutate(notes = ifelse(lowest_std < 10, 
+#                               paste0(df[1, 'analyte'], " lowest standard falls below 10 net mfi"), 
+#                               NA)) %>%
+#         mutate(lowest_std_mfi = ifelse(lowest_std >= 10,
+#                                        lowest_std, NA))
+# }
+# 
+# # if cv is NA or cv > 20 then invalidate results - one analyte dataframe should be passed in. Also need to specify identification variables so they don't get invalidated.
+# invalidateResultsCV <- function(one_analyte_df, id_vars) {
+#     result_vars <- setdiff(names(one_analyte_df), c(id_vars, 'cv', 'analyte'))
+#     
+#     check <- one_analyte_df %>%
+#         mutate(across(result_vars, ~ case_when(cv > 20 ~ NA, 
+#                                                is.na(cv) ~ NA,
+#                                                TRUE ~ .)))
+# }
+# 
+# # THIS FUNCTION IS GROSS, FIX THIS WHEN YOU HAVE TIME
+# # check out: https://stackoverflow.com/questions/55059761/r-mutate-multiple-columns-with-ifelse-condition
+# # Function that select correct number of standard points to include in curve, given analyte name
+# filterStandards <- function(df) {
+#     stds_5_6_7 <- c("Standard5", "Standard6", "Standard7")# keep standards 1-4
+#     stds_6_7 <- c("Standard6", "Standard7")# keep standards 1-5
+#     std_7 <- c("Standard7")# keep standards 1-6
+#     
+#     analyte <- df[1, 'analyte']
+#     
+#     filtered_stds <- if (analyte == 'CHI3L1'){
+#         filter(df, (!specimenID %in% stds_6_7))
+#     } 
+#     else if (analyte == 'BDNF') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'MPO') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'CRP') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'IGFBP-1') {
+#         filter(df, (!specimenID %in% stds_6_7))
+#     }
+#     else if (analyte == 'IGFBP-3') {
+#         filter(df, (!specimenID %in% stds_5_6_7))
+#     }
+#     else if (analyte == 'Leptin') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'TNF-a') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'VEGF') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'IL-1B') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'IL-6') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else if (analyte == 'IL-10') {
+#         filter(df, (!specimenID %in% std_7))
+#     }
+#     else {
+#         df
+#     }
+# }
+# 
+# # remove analyte prefix from vars to make manipulations easier
+# removeVarsAnalytePrefix <- function(df) {
+#     #df <- analyte_df[[1]]
+#     analyte <- as.character(df[1, 'analyte'])
+#     
+#     prefix <- paste0(analyte, '_')
+#     
+#     remove_df <- df %>%
+#         rename_all(~str_replace(., paste0(analyte, '_'), ""))
+# }
+# 
+# # function that creates separate data frames for each analyte
+# subsetAnalyte <- function(df, plate_data) {
+#     analyte_name <- df
+#     
+#     analyte_df <- plate_data %>%
+#         select(c('subjectID', 'specimenID', 'plasma', 'visitnum', 'date', 'batch', 'Location', 'dilution_factor', 'file_name', 'analytes', contains(analyte_name))) %>%
+#         #rename_at(vars(contains(analyte_name)), ~ str_replace(., paste0(analyte_name, "_"), "")) %>%
+#         mutate(analyte = analyte_name)
+# }
 
 ### REMOVE THESE FUNCTIONS #####################################################
 # Function that adds original .csv file name as column in dataframe 
--- step1.R
+++ step1.R
@@ -1,16 +1,47 @@
 ### Author: Selene Banuelos
 ### Date: 06/01/2023
-### Description: 
+### Description: step 1 of Luminex data cleaning pipeline
 
-# step 1
-################################################################################
+# step 1 -----------------------------------------------------------------------
 # setup
 library(dplyr)
 library(purrr)
 library(janitor)
 library(stringr)
 library(tidyr)
-source('code/step1-functions.R')
+source('code/step1-functions.R') # import helper functions
+
+# function used to reformat raw xPONENT data
+# path should be a string with the format: 'T2_4plex'
+reformatRawData <- function(path, 
+                            plasma # dataframe with plasma availability
+                            ){
+
+    # import all csv in path as data frames into a list
+    data_list <- importDataAsList(path, "\\.csv") %>%
+        # standardize spelling of analytes
+        lapply(function(df) df %>% 
+                   # correct all misspellings of CHI3L1 to 'CHI3L1'
+                   mutate_all(str_replace_all, "CH.*L1$", "CHI3L1") %>%
+                   # make capitalization uniform for IL-1B
+                   mutate_all(str_replace_all, "IL-1b", "IL-1B") %>%
+                   # make all variations of TNF-a uniform
+                   mutate_all(str_replace_all, "TNF.*a$", "TNF-a")
+        ) %>%
+        # change all NaN to NA in dataframe
+        lapply(function(df) mutate_all(df, ~ifelse(is.nan(.), NA, .))) %>%
+        # extract batch name, run date, and analyte information
+        lapply(extractBatchDateAnalytes) %>%
+        # extract net mfi, %CV, std concentration, and dilution factor data
+        lapply(extractAndCombineData) %>% 
+        # remove empty wells
+        lapply(filter, specimenID != 'blank') %>%
+        # Merge visit number to cleaned data (do i need this here?)
+        lapply(addVisitnum, plasma) %>%
+        # do i need to do this?
+        lapply(rename, dilution_factor = 'Dilution Factor')
+    
+}
 
 # import data
 # plasma availability data
